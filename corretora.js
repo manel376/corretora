e2bfdf5363956bd4c3d468da4dad5b47601d8a7f
emanuel <emanuelkguimaraes@gamil.com>
2024-08-06 09:30:47 -0300

    7

---
+++ corretora.js
@@ -0,0 +1,24 @@
+const prompt = require (prompt-Sync)
+
+const db = [];
+
+let proxId = 1;
+
+const model = (id = ) =>{
+    const nome = prompt("Nome:  ");
+
+
+
+
+        if(nome != " ") {
+            return {
+                id,
+                nome,
+            }
+        };
+
+        console.log ("dados invalidos")
+            const store = () =>{
+                const novo
+            }
+}
